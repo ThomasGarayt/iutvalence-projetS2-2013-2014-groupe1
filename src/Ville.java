a9b79d33e3d590a4fa216801b16a873c9fb43f1d
RomainVidal <Romain@Asus-Romain>
2014-05-21 20:19:10 +0200

    Ajout du constructeur de ville et de JavaDoc.
    Ajout du constructeur de ville qui prend une position en paramètre.
    Ajout de la JavaDoc.

--- src/Ville.java
+++ src/Ville.java
@@ -1,15 +1,46 @@
+/**
+ * @author Romain Une ville qui à un niveau et une position.
+ */
 public class Ville {
 
 	private int niveau;
-	
-	private Position position;
-	
+
+	private final Position position;
+
+	/**
+	 * Crée une ville avec une position et un niveau.
+	 * 
+	 * @param positionDeLaVille
+	 *            la position à laquelle on souhaite crée la ville.
+	 */
+	public Ville(Position positionDeLaVille) {
+		this.position = positionDeLaVille;
+		this.niveau = 1;
+	}
+
+	/**
+	 * Augmenter le niveau de la ville de 1 point.
+	 */
 	public void ameliorer() {
 		// Réduire la trésorerie ?
-		this.niveau ++;
+		this.niveau++;
 	}
-	
-	public Position obtenirPosition(){
+
+	/**
+	 * Permet d'obtenir la position de la ville.
+	 * 
+	 * @return La position de la ville.
+	 */
+	public Position obtenirPosition() {
 		return this.position;
+	}
+
+	/**
+	 * Permet d'obtenir le niveau de la ville.
+	 * 
+	 * @return Le niveau de la ville.
+	 */
+	public int obtenirNiveau() {
+		return this.niveau;
 	}
 }
