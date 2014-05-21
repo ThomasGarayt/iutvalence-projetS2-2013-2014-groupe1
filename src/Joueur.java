e4a27cfcf0fa840843c729a36f78aa5dd2b3ec14
RomainVidal <romain.vidal@iut-valence.fr>
2014-05-21 20:02:17 +0200

    Ajout de la JavaDoc de la classe Joueur.

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,19 +1,41 @@
+/**
+ * @author Romain Un Joueur, il possède une trésorerie et une liste d'unité.
+ */
 public class Joueur {
 
+	/**
+	 * Le nombre d'unité maximal que peut obtenir un joueur.
+	 */
 	public final static int NOMBRE_D_ARMEES_MAX = 0;
+
+	/**
+	 * La trésorerie de départ d'un joueur.
+	 */
+	public final static int TRESORERIE_DE_DEPART = 1000;
 
 	int tresorerie;
 	Unite[] armees;
 
+	/**
+	 * Crée un joueur avec une trésorerie par défaut et une liste d'unité vide.
+	 */
 	public Joueur() {
-		this.tresorerie = 0;
+		this.tresorerie = TRESORERIE_DE_DEPART;
 		this.armees = new Unite[NOMBRE_D_ARMEES_MAX];
 		for (int armeeCourante = 0; armeeCourante < NOMBRE_D_ARMEES_MAX; armeeCourante++)
 			this.armees[armeeCourante] = null;
 	}
 
+	/**
+	 * Permet au joueur de déplacer une de ses unités.
+	 * 
+	 * @param positionDeLuniteADeplacer
+	 *            La position de l'unité que le joueur souhaite déplacer.
+	 * @param positionFutureDeLUnite
+	 *            La position à laquelle le joueur souhaite déplacer son unité.
+	 */
 	public void deplacerUnite(Position positionDeLuniteADeplacer,
-			Position positionFutureDeLUnite) { //TODO Ne vérifie qu'un joueur !
+			Position positionFutureDeLUnite) { // TODO Ne vérifie qu'un joueur !
 		if (aUneUniteSurLaPosition(positionFutureDeLUnite))
 			; // Faire remonter une erreur ?
 		else {
@@ -22,20 +44,36 @@ public class Joueur {
 		}
 	}
 
-	
+	/**
+	 * Test si le joueur à une unité sur la case entrée en paramètre.
+	 * 
+	 * @param positionATester
+	 *            La position à laquelle ont souhaite savoir si le joueur
+	 *            possède une unité.
+	 * @return Vrai si le joueur possède une unité sur la position. Faux sinon.
+	 */
 	public boolean aUneUniteSurLaPosition(Position positionATester) {
 		for (int uniteCourante = 0; uniteCourante < NOMBRE_D_ARMEES_MAX; uniteCourante++)
 			if (this.armees[uniteCourante].occupeLaPosition(positionATester))
 				return true;
 		return false;
 	}
-	
-	public int TrouverUneUniteParLaPosition(Position positionDeLuniteADeplacer) {
+
+	/**
+	 * Sert à trouver la position dans le tableau d'unité du joueur grâce à une
+	 * position.
+	 * 
+	 * @param positionDeLuniteATrouver
+	 *            La position de l'unité que l'on souhaite trouvé.
+	 * @return la position de l'unité dans le tableau d'unités du joueur.
+	 */
+	public int TrouverUneUniteParLaPosition(Position positionDeLuniteATrouver) {
 		for (int uniteCourante = 0; uniteCourante < NOMBRE_D_ARMEES_MAX; uniteCourante++)
 			if (this.armees[uniteCourante]
-					.occupeLaPosition(positionDeLuniteADeplacer))
+					.occupeLaPosition(positionDeLuniteATrouver))
 				return uniteCourante;
 
-		return 0; // TODO renvoyer une erreur plutôt ( et la gérer tant qu'à faire ).
+		return 0; // TODO renvoyer une erreur plutôt ( et la gérer tant qu'à
+					// faire ).
 	}
 }
