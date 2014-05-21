9153694f35d6e40d5e1cabcca1870dcd9e91a284
RomainVidal <romain.vidal@iut-valence.fr>
2014-05-21 18:52:59 +0200

    Ajout d'un ensemble de méthodes permettant de déplacer des unités.
    Dans la classe Unite :
    	- Pour déplacer l'unité.
    	- Pour savoir si l'unité occupe une position
    Dans joueur :
    	- Pour savoir si le joueur à une unité sur la position.
    	- Pour trouver l'unité grâce à une position.
    	- Pour déplacer l'unité.

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,16 +1,41 @@
-
 public class Joueur {
 
 	public final static int NOMBRE_D_ARMEES_MAX = 0;
-	
+
 	int tresorerie;
 	Unite[] armees;
-	
-	
-	Joueur() {
+
+	public Joueur() {
 		this.tresorerie = 0;
 		this.armees = new Unite[NOMBRE_D_ARMEES_MAX];
 		for (int armeeCourante = 0; armeeCourante < NOMBRE_D_ARMEES_MAX; armeeCourante++)
 			this.armees[armeeCourante] = null;
+	}
+
+	public void deplacerUnite(Position positionDeLuniteADeplacer,
+			Position positionFutureDeLUnite) { //TODO Ne vérifie qu'un joueur !
+		if (aUneUniteSurLaPosition(positionFutureDeLUnite))
+			; // Faire remonter une erreur ?
+		else {
+			armees[TrouverUneUniteParLaPosition(positionDeLuniteADeplacer)]
+					.deplacerUnite(positionFutureDeLUnite);
+		}
+	}
+
+	
+	public boolean aUneUniteSurLaPosition(Position positionATester) {
+		for (int uniteCourante = 0; uniteCourante < NOMBRE_D_ARMEES_MAX; uniteCourante++)
+			if (this.armees[uniteCourante].occupeLaPosition(positionATester))
+				return true;
+		return false;
+	}
+	
+	public int TrouverUneUniteParLaPosition(Position positionDeLuniteADeplacer) {
+		for (int uniteCourante = 0; uniteCourante < NOMBRE_D_ARMEES_MAX; uniteCourante++)
+			if (this.armees[uniteCourante]
+					.occupeLaPosition(positionDeLuniteADeplacer))
+				return uniteCourante;
+
+		return 0; // TODO renvoyer une erreur plutôt ( et la gérer tant qu'à faire ).
 	}
 }
--- src/Unite.java
+++ src/Unite.java
@@ -4,24 +4,27 @@ public class Unite {
 	int vie;
 	TypeUnite type;
 	Position positionUnite;
-	
+
 	public Unite(TypeUnite type) {
 		this.type = type;
 		this.vie = type.getVie();
 		this.puissance = type.getPuissance();
 	}
-	
+
 	public boolean estVivante() {
 		return (vie > 0);
 	}
-	
-	public void changementDeVieRelatif(int vieAChanger){
+
+	public void changementDeVieRelatif(int vieAChanger) {
 		this.vie += vieAChanger;
 	}
-	
-	public void deplacerUnite(Position nouvellePosition)
-	{
-		// TEST A FAIRE AVANT
-	this.positionUnite = nouvellePosition;
+
+	public void deplacerUnite(Position nouvellePosition) {
+		// Test de la disponibilité de la case fait dans joueur.
+		this.positionUnite = nouvellePosition;
+	}
+
+	public boolean occupeLaPosition(Position positionATester) {
+		return (positionATester == this.positionUnite);
 	}
 }
