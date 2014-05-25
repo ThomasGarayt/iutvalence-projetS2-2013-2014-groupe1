8c0df799539e558b497b8d89914cd1d5542fe695
blue-fire <romain.vidal@iut-valence.com>
2014-05-25 12:14:54 +0200

    Création de la classe qui obtient le récapitulatif d'une unité.
    Cette classe crée le récapitulatif d’une unité passée en paramètre.
    Ou affiche un menu vide si aucune unité n’est passée en paramètre.

---
+++ src/ihmCivilisation/RecapUnite.java
@@ -0,0 +1,39 @@
+package ihmCivilisation;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import systemeCivilisation.Unite;
+
+public class RecapUnite extends JPanel {
+	
+	public RecapUnite() {
+		super();
+		
+		this.setLayout(new GridLayout(3,2));
+		this.add(new JLabel(" Niveau  "));
+		this.add(new JLabel("1") );
+		
+		this.add(new JLabel(" Vie  "));
+		this.add(new JLabel(" ") );
+		
+		this.add(new JLabel(" Deplacement  "));
+		this.add(new JLabel(" " ) );
+	}
+	
+	public RecapUnite(Unite unite) {
+		super();
+		
+		this.setLayout(new GridLayout(3,2));
+		this.add(new JLabel(" Niveau  "));
+		this.add(new JLabel("1") );
+		
+		this.add(new JLabel(" Vie  "));
+		this.add(new JLabel( Integer.toString(unite.obtenirVie()) ) );
+		
+		this.add(new JLabel(" Deplacement  "));
+		this.add(new JLabel( Integer.toString(unite.obtenirPointDeMouvements()) ) );
+	}
+}
