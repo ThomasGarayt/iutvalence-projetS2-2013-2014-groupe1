484d784d44dcdb55d17e265ed3bf2aef8839ca6e
Garayt Thomas <thomas.garayt@iut-valence.fr>
2014-05-27 14:24:09 +0200

    Rajout supplémentaire de Javadocs

--- src/systemeCivilisation/PartieDeCivilisation.java
+++ src/systemeCivilisation/PartieDeCivilisation.java
@@ -29,6 +29,20 @@ public class PartieDeCivilisation {
 		this.joueurDontCEstLeTour = 0;
 	}
 
+	
+	/**
+	 * Fonction permettant a un joueur d'attaquer un autre joueur
+	 * 
+	 * 
+	 * @param joueurAttaquant
+	 * 			Le joueur qui attaque
+	 * 
+	 * @param positionAttaquant
+	 * 			La position de l'unite attaquante
+	 * 
+	 * @param positionDefenseur
+	 * 			La position de l'unite qui defend
+	 */
 	public void attaquer(Joueur joueurAttaquant, Position positionAttaquant,
 			Position positionDefenseur) {
 		if ((this.carte.obtenirLUniteDeLaCase(positionAttaquant)
@@ -44,6 +58,21 @@ public class PartieDeCivilisation {
 
 	}
 
+	
+	
+	/**
+	 *  Fonction permettant de deplacer une unite
+	 * 
+	 * 
+	 * @param joueurCourant
+	 * 			Le joueur courant, qui est en train de jouer
+	 * 
+	 * @param positionDeLUnite
+	 * 			La position de l'unite qui va etre deplacer
+	 * 
+	 * @param positionFutureDeLUnite
+	 * 			La position voulue pour l'unite deplace
+	 */
 	public void deplacerUneUnite(Joueur joueurCourant,
 			Position positionDeLUnite, Position positionFutureDeLUnite) {
 		if ((this.carte.obtenirLUniteDeLaCase(positionDeLUnite).obtenirJoueur() == joueurCourant)
@@ -53,6 +82,19 @@ public class PartieDeCivilisation {
 					positionFutureDeLUnite);
 	}
 
+	
+	/**
+	 * Fonction permettant de prendre une ville
+	 * 
+	 * @param joueurAttaquant
+	 * 			Le joueur attaquant la ville
+	 * 
+	 * @param positionVille
+	 * 			La position de la ville attaquee
+	 * 
+	 * @param positionDeLUnite
+	 * 			La position de l'unite qui attaque
+	 */
 	public void prendreLaVille(Joueur joueurAttaquant, Position positionVille,
 			Position positionDeLUnite) {
 		if ((this.carte.obtenirLUniteDeLaCase(positionDeLUnite).obtenirJoueur() == joueurAttaquant)
@@ -63,6 +105,20 @@ public class PartieDeCivilisation {
 					.changerProprietaire(joueurAttaquant);
 	}
 
+	
+	/**
+	 * Fonction permettant d'ajouter une unite
+	 * 
+	 * 
+	 * @param joueurConstructeur
+	 * 			Le joueur qui souhaite ajouter une unitee
+	 * 
+	 * @param positionDeLunite
+	 * 			La position ou l'unite va etre ajouter
+	 * 
+	 * @param typeDeLUnite
+	 * 			Le type de l'unite ajoutee
+	 */
 	public void ajouterUneUnite(Joueur joueurConstructeur,
 			Position positionDeLunite, TypeUnite typeDeLUnite) {
 		if (this.carte.laCaseNecontientPasDUnite(positionDeLunite))
@@ -70,6 +126,13 @@ public class PartieDeCivilisation {
 					typeDeLUnite, joueurConstructeur));
 	}
 
+	
+	/**
+	 * Fonction créant la carte de jeu et mettant en place des unitees
+	 * 
+	 * @return carteDuMonde
+	 * 			La carte de jeu
+	 */
 	private Carte creationDeLaCarte() {
 		Carte carteDuMonde = new Carte();
 		carteDuMonde.ajouterUneUniteALaCase(new Position(2, 3), new Unite(
@@ -85,6 +148,10 @@ public class PartieDeCivilisation {
 		return carteDuMonde;
 	}
 
+	/**
+	 * Fonction permettant de finir le tour et de passer au joueur suivant
+	 * 
+	 */
 	public void finirLeTour() {
 		if (this.joueurDontCEstLeTour < NOMBRE_DE_JOUEUR - 1)
 			this.joueurDontCEstLeTour++;
@@ -92,10 +159,24 @@ public class PartieDeCivilisation {
 			this.joueurDontCEstLeTour = 0;
 	}
 
+	
+	/**
+	 * Fonction permettant d'obtenir le joueur courant
+	 * 
+	 * @return joueurDontCEstLeTour
+	 * 			Le joueur qui est en train de jouer
+	 */
 	public Joueur obtenirJoueurDontCEstLeTour() {
 		return this.joueurs[this.joueurDontCEstLeTour];
 	}
 
+	
+	/**
+	 * Retourne la carte du jeu
+	 * 
+	 * @return carte
+	 * 			La carte du jeu
+	 */
 	public Carte obtenirCarte() {
 		return this.carte;
 	}
